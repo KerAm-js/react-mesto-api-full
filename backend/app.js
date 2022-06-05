fd57ac96bd293d240e919a3bfca11ea9287f60ff
Amir Kerimov <95amirk@gmail.com>
2022-06-05 14:54:25 +0300

    feat: Add cors package

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const cors = require('cors');
 const { errors, celebrate, Joi } = require('celebrate');
 const userRouter = require('./routes/user');
 const cardRouter = require('./routes/card');
 const userController = require('./controllers/user');
-const cors = require('./middlewares/cors');
+// const cors = require('./middlewares/cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { pageNotFound } = require('./middlewares/pageNotFound');
 const { auth } = require('./middlewares/auth');
@@ -33,7 +34,10 @@ const { PORT = 3000 } = process.env;
 
 app.use(requestLogger);
 app.use(limiter);
-app.use(cors);
+app.use(cors({
+  origin: 'https://amir.projects.mesto.nomoredomains.sbs',
+  credentials: true,
+}));
 app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
